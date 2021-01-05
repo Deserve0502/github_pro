7034ff4f88844664e35728b4effa75a5e100c611
Deserv0502 <1347475525@qq.com>
2021-01-05 23:08:20 +0800

    node中文社区4.0

--- node_CMT/js/common.js
+++ node_CMT/js/common.js
@@ -1,26 +1,6 @@
-function iframeAdaptionPost(oCon,str) {
-    document.addEventListener('DOMContentLoaded', function () {
-        var Height =str + 'Height';
-        var Width = str + 'Width';
-        let width = oCon.clientWidth;
-        let height = oCon.clientHeight;
-        window.parent.postMessage([
-            Height+height,
-            Width+width,
-            
-        ]
-            , '*')
-    }, false)
-    Height = null;
-    Width = null;
-}
-
-function iframeAdaptionRecive(e){
-   let height =  e.data[0].replace(/[^0-9]/ig,"");
-   let width =  e.data[1].replace(/[^0-9]/ig,"");
-   return height;
-}
-
+/** 
+ * ajax请求
+*/
 function ajax(json) {
     if (window.XMLHttpRequest) {
         var ajax = new XMLHttpRequest();
@@ -37,11 +17,9 @@ function ajax(json) {
         ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         ajax.send(JsonToString(json.data));
     }
-
     ajax.onreadystatechange = function () {
         if (ajax.readyState == 4) {
             if (ajax.status >= 200 && ajax.status < 300 || ajax.status == 304) {
-                console.log(00000)
                 json.success(ajax.responseText);
             }
             else {
@@ -57,11 +35,16 @@ function ajax(json) {
         return arr.join('&');
     }
 }
+
+/**
+  * 时间戳格式化
+ */
 function getLastTime(containArrDate) {
     containArrDate = containArrDate.replace("T", " ");
     containArrDate = containArrDate.substr(0, 19)
     containArrDate = containArrDate.split(/[- : \/]/);
     let startDate = Date.parse(new Date(containArrDate[0], containArrDate[1] - 1, containArrDate[2],containArrDate[3], containArrDate[4], containArrDate[5]));
+    // 获取时间差值毫秒
     let dateDiff = new Date().getTime() - startDate;
     let dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000)); 
     let leave1 = dateDiff % (24 * 3600 * 1000);
@@ -76,18 +59,18 @@ function getLastTime(containArrDate) {
     for (let j = 0; j < timeFn.length; j++) {
         if (timeFn[j] !== '0') {
             if (j === 0) {
-                if(parseInt(timeFn[j]/365) !== 0){
+                if (parseInt(timeFn[j]/365) !== 0) {
                     lastTime = parseInt(timeFn[j]/365) + '年前';
                     break;
-                }else{
+                }else {
                     lastTime = timeFn[j] + '天前';
                 }
                 
                 break;
-            } else if (j === 1) {
+            }else if (j === 1) {
                 lastTime = timeFn[j] + '小时前';
                 break;
-            } else {
+            }else {
                 lastTime = timeFn[j] + '秒前';
                 break;
             }
@@ -96,23 +79,34 @@ function getLastTime(containArrDate) {
     }
     return lastTime;
 }
+
+/**
+ * iframe局部刷新
+ */
 function partRefresh() {
     ocontain.src = "../static/contain.html";
 }
 
+/**
+ * 分页封装
+ */
 function getPagination(ul,li,lsPage,endPage){
-    let startarr = ['«', '1', '2', '3', '4', '5', '...','»']; //起始样式
-    let endarr =  ['«', '...', '19', '20', '21','»']; //结束样式
+    // 起始样式
+    let startarr = ['«', '1', '2', '3', '4', '5', '...','»']; 
+    // 结束样式
+    let endarr =  ['«', '...', endPage-2, endPage-1, endPage,'»'];
     let arr = [];
     let str = '';
-    ls.setItem(lsPage,1)
+    // 自定义数组插入方法
     Array.prototype.insert = function (index, item) {
         this.splice(index, 0, item);
     };
+    ls.setItem(lsPage,1)
     arr =  copyArr(startarr);
     buildPa();
     liclick();
     getPaColor();
+    // 模版拼接按钮
     function buildPa() {
         for (let j = 0; j < arr.length; j++) {
             str += `
@@ -124,33 +118,31 @@ function getPagination(ul,li,lsPage,endPage){
         li = document.querySelectorAll(".contain_pagination > li");
         liclick();
     }
+    // 重建数组顺序
     function buildArr() {
         console.log(arr)
-        let chazhi = -2;
+        let difference = -2;
         for (let i = 2; i < arr.length - 2; i++) {
-            arr[i] = Number(ls.getItem(lsPage)) + Number(chazhi);
-            chazhi++;
+            arr[i] = Number(ls.getItem(lsPage)) + Number(difference);
+            difference++;
         }
     }
+    // 点击按钮事件
     function liclick() {
         for (let i = 0; i < li.length; i++) {
-            li[0].classList.add("iconfont");
-            li[arr.length -1].classList.add("iconfont");
-            li[0].classList.add("smallSize");
-            li[arr.length -1].classList.add("smallSize");
             li[i].onclick = function () {
                 ls.setItem(lsPage, li[i].innerHTML);
                 if (li[i].innerHTML >= 4 && li[i].innerHTML <= endPage) {
                     if (ls.getItem(lsPage) == (endPage - 2)) {
                         buildArr();
                        arr =  copyArr(endarr);
-                        arr.insert(2, 18);
-                        arr.insert(2, 17);
+                        arr.insert(2, endPage - 3);
+                        arr.insert(2, endPage - 4);
                         buildPa();
                     } else if (ls.getItem(lsPage) == (endPage - 1)) {
                         buildArr();
                         arr =  copyArr(endarr);
-                        arr.insert(2, 18);
+                        arr.insert(2, endPage - 3);
                         buildPa();
                     } else if (ls.getItem(lsPage) == (endPage)) {
                         buildArr();
@@ -179,7 +171,7 @@ function getPagination(ul,li,lsPage,endPage){
                 partRefresh();
             }
             li[arr.length - 1].onclick = function () {
-                ls.setItem(lsPage, 21);
+                ls.setItem(lsPage, endPage);
                 buildArr();
                 arr =  copyArr(endarr);
                 buildPa();
@@ -188,6 +180,7 @@ function getPagination(ul,li,lsPage,endPage){
             }
         }
     }
+    // 点击按钮颜色变化
     function getPaColor() {
         for (let j = 0; j < li.length; j++) {
             li[j].style.color = '#999';
@@ -196,6 +189,7 @@ function getPagination(ul,li,lsPage,endPage){
             }
         }
     }
+    // 深拷贝数组
     function copyArr(arr){
         let newarr = [];
         for(let i = 0;i < arr.length; i++){
@@ -206,6 +200,10 @@ function getPagination(ul,li,lsPage,endPage){
 
 }
 
-
+/**
+ * 常量提取
+ */
+const PX = 'px';
+let ls = localStorage;
 
 
